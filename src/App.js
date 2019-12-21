c2fa26e615f0e9604687eabe93b8d04634221c20
Alice Stang <alice@stacom.de>
2019-12-21 18:46:13 +0100

    json file update

--- src/App.js
+++ src/App.js
@@ -6,40 +6,22 @@ import {
   Col
 } from 'reactstrap';
 import './App.css';
-import BlogPost from './components/BlogPost';
-import dataReact from './components/contentBlogPost/react/GettingStartedWithReact';
-import dataExtjs from './components/contentBlogPost/extJs/GettingStartedWithExtjs';
-import dataJava from './components/contentBlogPost/java/GettingStartedWithJava';
-import dataArduino from './components/contentBlogPost/arduino/GettingStartedWithArduino';
+import BlogPost from './components/Layout/BlogPost';
+import blogPostJsonList from './components/contentBlogPost/BlogPosts';
 
 export default class App extends React.Component {
   
   constructor() {
     super();
-    let blogPostReact = dataReact.blogPost;
-    let blogPostExtJs = dataExtjs.blogPost;
-    let blogPostJava = dataJava.blogPost;
-    let blogPostArduino = dataArduino.blogPost;
+    let i = 0;
+    let blogPostArray = [];
+
+    for ( i; i < blogPostJsonList.size ; i += 1 ) {
+      blogPostArray.push(blogPostJsonList[i]);
+    }
 
     this.state = {
-      blogPosts: [
-        {
-          topic: blogPostReact.topic,
-          content: blogPostReact.content
-        },
-        {
-          topic: blogPostExtJs.topic,
-          content: blogPostExtJs.content
-        },
-        {
-          topic: blogPostJava.topic,
-          content: blogPostJava.content
-        },
-        {
-          topic: blogPostArduino.topic,
-          content: blogPostArduino.content
-        }
-      ]
+      blogPosts: blogPostJsonList
     }
   }
 
